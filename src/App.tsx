b7f3e863711dd558bf074f33da3398dc393c0098
Haydenull <brooklyn1984@qq.com>
2022-02-19 16:02:52 +0800

    wip: add prev next button

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,50 @@ import { useEffect, useRef, useState } from 'react'
 import Calendar from 'tui-calendar'
 import { Button, Select } from 'antd'
 import { LeftOutlined, RightOutlined } from '@ant-design/icons'
+import day from 'dayjs'
 import 'tui-calendar/dist/tui-calendar.css'
 import 'antd/dist/antd.css'
 import './App.css'
 
 const DEFAULT_OPTIONS = {
   defaultView: 'month',
-  task: true,
+  taskView: true,
+  scheduleView: true,
 }
 
 const App: React.FC<{ env: string }> = ({ env }) => {
 
-  const [showDate, setShowDate] = useState()
+  const [showDate, setShowDate] = useState<string>()
   const calendarRef = useRef<Calendar>()
+
+  const changeShowDate = () => {
+    if (calendarRef.current) {
+      const dateRangeStart = day(calendarRef.current.getDateRangeStart().getTime())
+      const dateRangeEnd = day(calendarRef.current.getDateRangeEnd().getTime())
+      if (dateRangeStart.isSame(dateRangeEnd, 'day')) {
+        setShowDate(dateRangeStart.format('YYYY-MM-DD'))
+      } else {
+        setShowDate(dateRangeStart.format('YYYY-MM-DD') + ' ~ ' + dateRangeEnd.format('YYYY-MM-DD'))
+      }
+    }
+  }
 
   const onViewChange = (value: string) => {
     console.log('[faiz:] === onViewChange',value, calendarRef.current)
     calendarRef.current?.changeView(value)
+    changeShowDate()
   }
   const onClickToday = () => {
     calendarRef.current?.today()
+    changeShowDate()
+  }
+  const onClickPrev = () => {
+    calendarRef.current?.prev()
+    changeShowDate()
+  }
+  const onClickNext = () => {
+    calendarRef.current?.next()
+    changeShowDate()
   }
 
   useEffect(() => {
@@ -33,25 +57,26 @@ const App: React.FC<{ env: string }> = ({ env }) => {
       //   // }
       // }
     })
+    changeShowDate()
   }, [])
 
   return (
     <div className="w-screen h-screen flex items-center justify-center text-white">
       <div className="w-screen h-screen fixed top-0 left-0" onClick={() => logseq.hideMainUI()}></div>
-      <div className="w-5/6 h-5/6 bg-gradient-to-tr from-yellow-400 via-red-500 to-pink-500 flex flex-col overflow-hidden">
-        <div className="p-1">
+      <div className="w-5/6 h-5/6 flex flex-col overflow-hidden">
+        <div className="mb-2 flex items-center">
           <Select defaultValue={DEFAULT_OPTIONS.defaultView} onChange={onViewChange} style={{ width: '150px' }}>
             <Select.Option value="day">Daily</Select.Option>
-            <Select.Option value="week">weekly</Select.Option>
-            <Select.Option value="month">monthly</Select.Option>
+            <Select.Option value="week">Weekly</Select.Option>
+            <Select.Option value="month">Monthly</Select.Option>
           </Select>
 
-          <Button shape="round" onClick={onClickToday}>Today</Button>
+          <Button className="ml-4" shape="round" onClick={onClickToday}>Today</Button>
 
-          <Button shape="circle" icon={<LeftOutlined />}></Button>
-          <Button shape="circle" icon={<RightOutlined />}></Button>
+          <Button className="ml-4" shape="circle" icon={<LeftOutlined />} onClick={onClickPrev}></Button>
+          <Button className="ml-1" shape="circle" icon={<RightOutlined />} onClick={onClickNext}></Button>
 
-          <div>{{ showDate }}</div>
+          <span className="ml-4 text-black text-xl">{ showDate }</span>
         </div>
         <div id="calendar"></div>
       </div>
