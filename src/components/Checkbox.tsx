86ebc0322a0335d67f7950e0e4bfc600cda77e65
Haydenull <brooklyn1984@qq.com>
2022-05-14 22:17:14 +0800

    feat: add check all button

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 const Checkbox: React.FC<{
   checked?: boolean
   color?: string
+  indeterminate?: boolean
   onChange?: (checked: boolean) => void
   [props: string]: any
-}> = ({ checked, color, onChange, children, ...props }) => {
+}> = ({ checked, color, indeterminate, onChange, children, ...props }) => {
+  // TODO: add indeterminate state
   return (
     <div onClick={() => onChange?.(!checked)} {...props}>
       <span className={`check-box mr-2 ${checked ? 'checked' : ''}`} style={{ backgroundColor: color }}></span>
