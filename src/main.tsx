b96eaf04715d8f883f57ea1921ded6788771d7a6
Haydenull <brooklyn1984@qq.com>
2022-08-04 08:00:11 +0800

    fix: page name fault when Add this page to agenda project

--- src/main.tsx
+++ src/main.tsx
@@ -200,12 +200,15 @@ if (isDevelopment) {
       })
       logseq.showMainUI()
     }
-    logseq.App.registerPageMenuItem('Agenda: Add this page to agenda project', ({ page }) => {
+    logseq.App.registerPageMenuItem('Agenda: Add this page to agenda project', async ({ page }) => {
+      const pageData = await logseq.Editor.getPage(page)
+      if (!pageData) return logseq.UI.showMsg('Page not found', 'error')
       const originalProjectList = logseq.settings?.projectList || []
-      if (originalProjectList.find(project => project.id === page)) return logseq.App.showMsg('This Page is already in agenda project', 'warning')
+      const pageName = pageData.originalName
+      if (originalProjectList.find(project => project.id === pageName)) return logseq.UI.showMsg('This Page is already in agenda project', 'warning')
       const newProject = {
         ...DEFAULT_PROJECT,
-        id: page,
+        id: pageName,
       }
       // hack https://github.com/logseq/logseq/issues/4447
       logseq.updateSettings({projectList: 1})
