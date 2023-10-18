1e72938d1483e4a839c28ecb7f9a9900ea0eb7ab
Hayden Chen <brooklyn1984@qq.com>
2023-10-18 15:59:18 +0800

    feat(agenda3): :sparkles: compitablee style for empty backlog

--- src/pages/NewDashboard/components/Backlog.tsx
+++ src/pages/NewDashboard/components/Backlog.tsx
@@ -1,6 +1,6 @@
 import { CaretRightOutlined } from '@ant-design/icons'
 import { Draggable } from '@fullcalendar/interaction'
-import { Collapse } from 'antd'
+import { Collapse, Empty } from 'antd'
 import clsx from 'clsx'
 import { useAtom } from 'jotai'
 import { useEffect, useRef } from 'react'
@@ -27,39 +27,51 @@ const Backlog = () => {
       <div className="h-[44px] flex items-center gap-1.5 relative after:shadow after:absolute after:bottom-0 after:w-full after:h-1">
         <BsArchive /> Backlog
       </div>
-      <div className="flex flex-col gap-2 flex-1 overflow-auto pt-2" ref={taskContainerRef}>
-        <Collapse
-          accordion
-          bordered={false}
-          expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
-          style={{ background: '#f9fafb' }}
-          items={categorizedTasks.map((project) => ({
-            key: project.project.originalName,
-            label: project.project.originalName,
-            children: (
-              <div className="flex flex-col gap-2">
-                {project.tasks.map((task) => (
-                  <div
-                    key={task.id}
-                    className="border rounded px-2 py-2 text-sm text-gray-600 break-all droppable-task-element bg-[#f9fafb]"
-                    data-event={JSON.stringify({
-                      id: task.id,
-                      title: task.title,
-                    })}
-                  >
-                    {task.title}
-                  </div>
-                ))}
-              </div>
-            ),
-            style: {
-              marginBottom: 10,
-              borderRadius: 4,
-              border: 'none',
-              background: '#f0f0f0',
-            },
-          }))}
-        />
+      <div
+        className={clsx('flex flex-col gap-2 flex-1 overflow-auto pt-2', {
+          'justify-center': categorizedTasks?.length <= 0,
+        })}
+        ref={taskContainerRef}
+      >
+        {categorizedTasks?.length > 0 ? (
+          <Collapse
+            accordion
+            bordered={false}
+            expandIcon={({ isActive }) => <CaretRightOutlined rotate={isActive ? 90 : 0} />}
+            style={{ background: '#f9fafb' }}
+            items={categorizedTasks.map((project) => ({
+              key: project.project.originalName,
+              label: project.project.originalName,
+              children: (
+                <div className="flex flex-col gap-2">
+                  {project.tasks.map((task) => (
+                    <div
+                      key={task.id}
+                      className="border rounded px-2 py-2 text-sm text-gray-600 break-all droppable-task-element bg-[#f9fafb]"
+                      data-event={JSON.stringify({
+                        id: task.id,
+                        title: task.title,
+                      })}
+                    >
+                      {task.title}
+                    </div>
+                  ))}
+                </div>
+              ),
+              style: {
+                marginBottom: 10,
+                borderRadius: 4,
+                border: 'none',
+                background: '#f0f0f0',
+              },
+            }))}
+          />
+        ) : (
+          <Empty
+            image="https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg"
+            imageStyle={{ height: 60, display: 'flex', justifyContent: 'center' }}
+          />
+        )}
       </div>
     </div>
   )
