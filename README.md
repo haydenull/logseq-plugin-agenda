20b665c21e187b7f892753a6714079745952f739
Hayden Chen <brooklyn1984@qq.com>
2023-11-07 13:03:38 +0800

    docs(agenda3): :memo: supplement docs

--- README.md
+++ README.md
@@ -4,25 +4,37 @@
 [![latest release version](https://img.shields.io/github/v/release/haydenull/logseq-plugin-agenda)](https://github.com/haydenull/logseq-plugin-agenda/releases)
 [![License](https://img.shields.io/github/license/haydenull/logseq-plugin-agenda?color=blue)](https://github.com/haydenull/logseq-plugin-agenda/blob/main/LICENSE)
 
-Click [here](https://haydenull.github.io/logseq-plugin-agenda/) for the full documentation
+To access the documentation, please refer to the following links based on the version you are using.
+
+For version 2.0, you can find the documentation at https://haydenull.github.io/logseq-plugin-agenda/.
+
+If you are using version 3.0, please visit https://haydenut.notion.site/Agenda3-ef115e277c864de3b2679d6bda0e6376 for the documentation
 
 If you want to contribute, please visit [contribution guide](./CONTRIBUTING.md)
 
 <a href="https://www.buymeacoffee.com/haydenull"><img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=haydenull&button_colour=40DCA5&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" /></a>
 
-<a href="https://afdian.net/a/haydenull"><img width="235" src="https://pic1.afdiancdn.com/static/img/welcome/button-sponsorme.jpg" alt=""></a >
+<a href="https://afdian.net/a/haydenull"><img width="235" src="https://pic1.afdiancdn.com/static/img/welcome/button-sponsorme.jpg"/></a>
 
 ## 🎨 Features
-- Supports dashboard
+- Supports Kanban
+- Supports Calendar
+- Supports agenda sharing
+- Supports use as a webpage or plugin
+<!-- - Supports dashboard
 - Supports sidebar
 - Supports dark mode
 - Supports calendar view
 - Supports gantt view
 - Supports timeline view
 - Supports subscription calendar(basic event)
-- Supports pomodoro timer
+- Supports pomodoro timer -->
 
-![dashboard-light](./screenshots/dashboard-light.png)
+![kanban](./screenshots/agenda3-kanban.jpeg)
+![calendar-month](./screenshots/agenda3-calendar-moonth.png)
+![calendar-week](./screenshots/agenda3-calendar-week.png)
+![ics](./screenshots/agenda3-ics.png)
+<!-- ![dashboard-light](./screenshots/dashboard-light.png)
 ![dashboard-purple](./screenshots/dashboard-purple.png)
 ![dashboard-dark](./screenshots/dashboard-dark.png)
 ![sidebar](./screenshots/sidebar.jpg)
@@ -30,10 +42,7 @@ If you want to contribute, please visit [contribution guide](./CONTRIBUTING.md)
 ![WeeklyView](./screenshots/weeklyView.png)
 ![GanttView](./screenshots/gantt-advanced.png)
 ![Timeline](./screenshots/timeline.png)
-![Pomodoro](./screenshots/pomodoro.png)
-<!-- ![settings](./screenshots/settings.png)
-![query](./screenshots/query.png)
-![agenda](./screenshots/modifyAgenda.png) -->
+![Pomodoro](./screenshots/pomodoro.png) -->
 
 ## Star History
 
--- src/pages/NewDashboard/components/SettingsModal.tsx
+++ src/pages/NewDashboard/components/SettingsModal.tsx
@@ -1,9 +1,10 @@
 import { CopyOutlined, QuestionCircleOutlined, QuestionOutlined } from '@ant-design/icons'
-import { Checkbox, Input, Menu, Modal, Tooltip, Typography, message } from 'antd'
+import { Button, Checkbox, Input, Menu, Modal, Tooltip, Typography, message } from 'antd'
 import type { MenuProps } from 'antd'
 import copy from 'copy-to-clipboard'
 import { useState } from 'react'
 
+import logo from '@/assets/logo.png'
 import useSettings from '@/hooks/useSettings'
 import { cn } from '@/util/util'
 
@@ -32,6 +33,10 @@ const tabs = [
     key: 'shareAgenda',
     label: 'Share Agenda',
   },
+  {
+    key: 'about',
+    label: 'About',
+  },
 ] as const
 const SettingsModal = ({ children }: { children?: React.ReactNode }) => {
   const [open, setOpen] = useState(false)
@@ -48,6 +53,15 @@ const SettingsModal = ({ children }: { children?: React.ReactNode }) => {
     message.success('🎉 Copied!')
   }
 
+  const onClickLink = (link: string) => {
+    if (import.meta.env.VITE_MODE === 'plugin') {
+      logseq.App.openExternalLink(link)
+    } else {
+      window.open(link, '_blank')
+    }
+    return false
+  }
+
   const renderForm = () => {
     switch (activeTab) {
       case 'shareAgenda':
@@ -59,6 +73,7 @@ const SettingsModal = ({ children }: { children?: React.ReactNode }) => {
                 <div className="text-gray-500">Github Repo</div>
                 <Input
                   className="w-[300px]"
+                  placeholder="username/repo"
                   value={settings.ics?.repo}
                   onChange={(e) => onChange('ics.repo', e.target.value)}
                 />
@@ -67,6 +82,7 @@ const SettingsModal = ({ children }: { children?: React.ReactNode }) => {
                 <div className="text-gray-500">Github Access Token</div>
                 <Input.Password
                   className="w-[300px]"
+                  placeholder="github access token"
                   value={settings.ics?.token}
                   onChange={(e) => onChange('ics.token', e.target.value)}
                 />
@@ -110,6 +126,36 @@ const SettingsModal = ({ children }: { children?: React.ReactNode }) => {
                 >
                   Hide Completed Tasks
                 </Checkbox>
+              </div>
+            </div>
+          </>
+        )
+      case 'about':
+        return (
+          <>
+            <div className="h-14 pl-4 flex items-center font-semibold text-lg border-b">About</div>
+            <div className="flex flex-col justify-center items-center pb-6">
+              <img src={logo} className="w-20 mt-6" />
+              <h1 className="text-xl">Agenda</h1>
+              <div className="text-gray-400">version: v{__APP_VERSION__}</div>
+              <a onClick={() => onClickLink('https://haydenut.notion.site/Agenda3-ef115e277c864de3b2679d6bda0e6376')}>
+                User Manual
+              </a>
+              <div className="text-xs text-gray-400 w-96">
+                Please note that the beta version is still under development and may contain bugs. We encourage you to
+                test it out and provide feedback on any issues or suggestions on our GitHub page. Your input is valuable
+                in ensuring a stable and polished final release.
+              </div>
+              <div className="mt-2 flex flex-col gap-2">
+                <a className="w-[190px]" onClick={() => onClickLink('https://www.buymeacoffee.com/haydenull')}>
+                  <img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=haydenull&button_colour=40DCA5&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" />
+                </a>
+                <a
+                  className="w-[190px] bg-[#946ce6] flex items-center justify-center rounded-lg"
+                  onClick={() => onClickLink('https://afdian.net/a/haydenull')}
+                >
+                  <img width="156" src="https://pic1.afdiancdn.com/static/img/welcome/button-sponsorme.jpg" alt="" />
+                </a>
               </div>
             </div>
           </>
