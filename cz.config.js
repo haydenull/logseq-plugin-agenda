c5ca4d925f49d55169067943234292a478aa749f
Hayden Chen <hayden.chen.b612@gmail.com>
2023-11-09 08:28:46 +0800

    chore: :wrench: update cz scopes

--- cz.config.js
+++ cz.config.js
@@ -1,16 +1,5 @@
 /** @type {import('czg').CommitizenGitOptions} */
 module.exports = {
   ...require('@haydenull/fabric/cz'),
-  scopes: [
-    'dashboard',
-    'calendar',
-    'gantt',
-    'timeline',
-    'daily-log',
-    'review',
-    'pomodoro',
-    'sidebar',
-    'settings',
-    'agenda3',
-  ],
+  scopes: ['settings', 'calendar', 'kanban', 'timebox', 'backlog', 'share', 'agenda2'],
 }
