6557e25a21c38a1e1990e5d7df0f877200627ad3
Hayden Chen <hayden.chen.b612@gmail.com>
2023-11-09 08:24:53 +0800

    fix(agenda3): :bug: can't show error msg when uploading ics file

--- src/services/ics.ts
+++ src/services/ics.ts
@@ -1,10 +1,18 @@
 export const uploadIcsHttp = async ({ repo, token, file }: { repo: string; token: string; file: string }) => {
-  return fetch(`https://agenda-ics.haydenhayden.com`, {
-    method: 'POST',
-    body: JSON.stringify({
-      repo,
-      token,
-      file,
-    }),
+  return new Promise((resolve, reject) => {
+    fetch(`https://agenda-ics.haydenhayden.com`, {
+      method: 'POST',
+      body: JSON.stringify({
+        repo,
+        token,
+        file,
+      }),
+    }).then(async (response) => {
+      const data = (await response.json()) as { message: string }
+      if (!response.ok) {
+        reject(data)
+      }
+      resolve(data)
+    })
   })
 }
